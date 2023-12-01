e53862b4e9082a73728a8f613016bcc7e63170b2
Robert MacLean <robert@sadev.co.za>
2023-12-01 12:54:09 +0200

    test changes for part 2

--- day1/index.js
+++ day1/index.js
@@ -14,24 +14,65 @@ const readInputAsText = async () => {
     }
 }
 
+const numberTests = [
+    { testString: "zero", value: "0" },
+    { testString: "one", value: "1" },
+    { testString: "two", value: "2" },
+    { testString: "three", value: "3" },
+    { testString: "four", value: "4" },
+    { testString: "five", value: "5" },
+    { testString: "six", value: "6" },
+    { testString: "seven", value: "7" },
+    { testString: "eight", value: "8" },
+    { testString: "nine", value: "9" }
+]
+
+/**
+ * @param {string} input
+ * @returns {string}
+ */
+const slidingWindowWordsToNumbers = (input) => {
+    let currentWindow = ""
+    let result = ""
+
+    input.split('').forEach((char) => {
+        currentWindow += char
+        let digit = undefined
+        let testIndex = 0
+        while (!digit && testIndex < 10) {
+            const numberTest = numberTests[testIndex]
+            const foundIndex = currentWindow.indexOf(numberTest.testString)
+            if (foundIndex > -1) {
+                digit = { length: numberTest.testString.length, value: numberTest.value }
+            }
+
+            testIndex++
+        }
+
+        if (digit) {
+            result += currentWindow.substring(0, currentWindow.length - digit.length) + digit.value
+            currentWindow = ""
+        }
+    })
+
+    return result += currentWindow
+}
+
 const main = async () => {
     const data = (await readInputAsText())
         .split('\n')
+        .map(line => line.toLocaleLowerCase())
         .map(line => {
-            return line
+            return slidingWindowWordsToNumbers(line)
                 .split('')
                 .filter(char => Number.isInteger(+char))
-        })
-        .map(numbers => {
-            console.log(numbers)
-            return numbers
         })
         .filter(numbers => numbers.length > 0)
         .map(numbers => {
             if (numbers.length === 1) {
                 return +(numbers[0] + numbers[0])
             } else {
-                return +(numbers[0] + numbers[numbers.length-1])
+                return +(numbers[0] + numbers[numbers.length - 1])
             }
         })
         .reduce((prev, curr) => prev += curr, 0)
