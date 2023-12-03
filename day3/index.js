fd817e043c6caa349d5800d5cbc491da578ab1ee
Robert MacLean <robert@sadev.co.za>
2023-12-03 08:39:31 +0200

    day3 setup

---
+++ day3/index.js
@@ -0,0 +1,93 @@
+const fs = require('node:fs/promises')
+
+/**
+ * read an input
+ * @returns {Promise<String>}
+ */
+const readInputAsText = async () => {
+    try {
+        const data = await fs.readFile('./day3/input.txt', { encoding: 'utf8' })
+        return data
+    } catch (err) {
+        console.error(err)
+        return ""
+    }
+}
+
+const isSymbol = /[^\.|\d]/
+const isDigit = /\d/
+
+const main = async () => {
+    const data = await readInputAsText()
+    const lines = data
+        .split('\n')
+        .filter(line => line);
+
+    const symbols = []
+    const numbers = []
+
+    for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+        const line = lines[lineIndex];
+        const chars = line.split('')
+        for (let charIndex = 0; charIndex < chars.length; charIndex++) {
+            const char = chars[charIndex];
+            if (char.match(isSymbol)) {
+                symbols.push({char, lineIndex, charIndex})
+            }
+        }
+    }
+
+    for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+        const line = lines[lineIndex];
+        const chars = line.split('')
+        let currentDigits = ''
+        for (let charIndex = 0; charIndex < chars.length; charIndex++) {
+            const char = chars[charIndex];
+            if (char.match(isDigit)) {
+                currentDigits += char
+            } else {
+                if (currentDigits) {
+                    numbers.push({
+                        digit: +currentDigits,
+                        lineIndex,
+                        endChatIndex: charIndex,
+                        startCharIndex: charIndex - currentDigits.length
+                    })
+                    currentDigits = ''
+                }
+            }
+        }
+    }
+
+    const numbersWithTouchingSymbolInfo = numbers.map(number => {
+        let touchingSymbol = false
+        for (const symbol of symbols) {
+            if (symbol.lineIndex -1 === number.lineIndex ||
+                symbol.lineIndex === number.lineIndex ||
+                symbol.lineIndex + 1 === number.lineIndex) {
+                    const lowerBound = number.startCharIndex - 1
+                    const upperBound = number.endChatIndex + 1
+
+                    touchingSymbol = symbol.charIndex >= lowerBound && symbol.charIndex <= upperBound
+                }
+
+            if (touchingSymbol) {
+                break
+            }
+        }
+
+        return {
+            ...number,
+            touchingSymbol,
+        }
+    })
+
+    const result = numbersWithTouchingSymbolInfo
+        .filter(v => v.touchingSymbol)
+        .map(v => v.digit)
+        .reduce((prev, curr) => prev += curr, 0)
+
+    console.log(result)
+}
+
+main()
